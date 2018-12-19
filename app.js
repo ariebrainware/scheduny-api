386944cfccd7acaf153cd41530026eda821c5833
Arie Brainware <publicu001@gmail.com>
2018-12-19 11:40:23 +0700

    Init commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const favicon = require('serve-favicon')
+
+const indexRouter = require('./api/index');
+
+const app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(favicon(path.join(__dirname,'favicon.ico')))
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Send the error page
+  res.status(err.status || 500);
+  res.status(500).send('error');
+});
+
+module.exports = app;
